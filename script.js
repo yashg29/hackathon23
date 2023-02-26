57b8bf21f2f61b19bffa93d7203199bfc8fe82eb
YASH GOHIL <104823881+yashg29@users.noreply.github.com>
2023-02-25 18:34:42 -0600

    added portfolio

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+let prices = [];
+let btccount=1;
+let ethcount=1;
+let bnbcount=1;
+let xrpcount=1;
+let adacount=1;
+let okbcount=1;
+let dogecount=1;
+let maticount=1;
+let solcount=1;
+let dotcount=1;
+let shibcount=1;
+let ltccount=1;
+
 // Fetch data for Bitcoin
 fetch('https://api.coingecko.com/api/v3/coins/bitcoin')  
 .then(response => response.json())
 .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -10,16 +25,18 @@ fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
     document.getElementById("btcmcap").append (" $"+mcap);
     document.getElementById("btcpc24H").append(" "+pChange24h+" %")
     document.getElementById("btcvol").append(" $"+volumeB);
+    
   })
   .catch(error => {
     console.error(error);
   });
 
-// Fetch data for Ethereum
+  // Fetch data for Ethereum
 fetch('https://api.coingecko.com/api/v3/coins/ethereum')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -37,6 +54,7 @@ fetch('https://api.coingecko.com/api/v3/coins/binancecoin')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -54,6 +72,7 @@ fetch('https://api.coingecko.com/api/v3/coins/ripple')
 .then(response => response.json())
 .then(data => {
   const price = data.market_data.current_price.usd;
+  prices.push(price);
   const mcap = data.market_data.market_cap.usd;
   const pChange24h = data.market_data.price_change_percentage_24h;
   const volumeB=data.market_data.total_volume.usd;
@@ -71,6 +90,7 @@ fetch('https://api.coingecko.com/api/v3/coins/cardano')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -88,6 +108,7 @@ fetch('https://api.coingecko.com/api/v3/coins/okb')
 .then(response => response.json())
 .then(data => {
   const price = data.market_data.current_price.usd;
+  prices.push(price);
   const mcap = data.market_data.market_cap.usd;
   const pChange24h = data.market_data.price_change_percentage_24h;
   const volumeB=data.market_data.total_volume.usd;
@@ -105,6 +126,7 @@ fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -122,6 +144,7 @@ fetch('https://api.coingecko.com/api/v3/coins/matic-network')
 .then(response => response.json())
 .then(data => {
   const price = data.market_data.current_price.usd;
+  prices.push(price);
   const mcap = data.market_data.market_cap.usd;
   const pChange24h = data.market_data.price_change_percentage_24h;
   const volumeB=data.market_data.total_volume.usd;
@@ -139,6 +162,7 @@ fetch('https://api.coingecko.com/api/v3/coins/solana')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -156,6 +180,7 @@ fetch('https://api.coingecko.com/api/v3/coins/polkadot')
 .then(response => response.json())
 .then(data => {
   const price = data.market_data.current_price.usd;
+  prices.push(price);
   const mcap = data.market_data.market_cap.usd;
   const pChange24h = data.market_data.price_change_percentage_24h;
   const volumeB=data.market_data.total_volume.usd;
@@ -173,6 +198,7 @@ fetch('https://api.coingecko.com/api/v3/coins/shiba-inu')
   .then(response => response.json())
   .then(data => {
     const price = data.market_data.current_price.usd;
+    prices.push(price);
     const mcap = data.market_data.market_cap.usd;
     const pChange24h = data.market_data.price_change_percentage_24h;
     const volumeB=data.market_data.total_volume.usd;
@@ -190,6 +216,7 @@ fetch('https://api.coingecko.com/api/v3/coins/litecoin')
 .then(response => response.json())
 .then(data => {
   const price = data.market_data.current_price.usd;
+  prices.push(price);
   const mcap = data.market_data.market_cap.usd;
   const pChange24h = data.market_data.price_change_percentage_24h;
   const volumeB=data.market_data.total_volume.usd;
@@ -201,3 +228,75 @@ fetch('https://api.coingecko.com/api/v3/coins/litecoin')
 .catch(error => {
   console.error(error);
 });
+
+
+
+setTimeout(() => {
+
+    //b2.addEventListener('click', () => PopUp());
+
+    document.getElementById("btcpriceh").innerHTML = "$"+(btccount*prices[0]);
+    document.getElementById("ethpriceh").innerHTML = "$"+(ethcount*prices[1]);
+    document.getElementById("bnbpriceh").innerHTML = "$"+(bnbcount*prices[2]);
+    document.getElementById("xrppriceh").innerHTML = "$"+(xrpcount*prices[3]);
+    document.getElementById("adapriceh").innerHTML = "$"+(adacount*prices[4]);
+    document.getElementById("okbpriceh").innerHTML = "$"+(okbcount*prices[5]);
+    document.getElementById("dogepriceh").innerHTML = "$"+(dogecount*prices[6]);
+    document.getElementById("maticpriceh").innerHTML = "$"+(maticount*prices[7]);
+    document.getElementById("solpriceh").innerHTML = "$"+(solcount*prices[8]);
+    document.getElementById("dotpriceh").innerHTML = "$"+(dotcount*prices[9]);
+    document.getElementById("shibpriceh").innerHTML = "$"+(shibcount*prices[10]);
+    document.getElementById("ltcpriceh").innerHTML = "$"+(ltccount*prices[11]);
+
+  }, 3000);
+
+
+  function PopUp(){
+        // Create the prompt dialog box
+        const dialogBox = document.createElement('div');
+        dialogBox.classList.add('prompt-box');
+    
+        // Create the label
+        const label = document.createElement('label');
+        label.classList.add('prompt-label');
+        label.textContent = 'Please enter your holdings:';
+    
+        // Create the input field
+        const input = document.createElement('input');
+        input.classList.add('prompt-input');
+        input.type = 'text';
+    
+        // Create the OK button
+        const okButton = document.createElement('button');
+        okButton.textContent = 'OK';
+        okButton.addEventListener('click', () => {
+          // Get the value of the input field and do something with it
+          const holdings = input.value;
+          // ...
+          // Remove the dialog box from the DOM
+          document.body.removeChild(dialogBox);
+        });
+    
+        // Create the Cancel button
+        const cancelButton = document.createElement('button');
+        cancelButton.textContent = 'Cancel';
+        cancelButton.addEventListener('click', () => {
+          // Remove the dialog box from the DOM
+          document.body.removeChild(dialogBox);
+        });
+    
+        // Add the label, input field, and buttons to the dialog box
+        dialogBox.appendChild(label);
+        dialogBox.appendChild(input);
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('prompt-buttons');
+        buttonsContainer.appendChild(okButton);
+        buttonsContainer.appendChild(cancelButton);
+        dialogBox.appendChild(buttonsContainer);
+    
+        // Add the dialog box to the DOM
+        document.body.appendChild(dialogBox);
+    
+        // Set the focus to the input field
+        input.focus();
+  }
